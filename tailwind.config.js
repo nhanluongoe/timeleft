4596cd4185376fda4cd7bc75e2b9a3926588b8f4
nhanluongoe <nhanlt18@gmail.com>
2024-04-25 17:17:08 +0700

    fix: update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js, vue}"],
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
