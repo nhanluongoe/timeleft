593f8c32a026075ef4b664d4bf79884e5b988cc4
nhanluongoe <nhanlt18@gmail.com>
2024-04-28 17:02:28 +0700

    build: update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/timeleft/",
+  base: "/timeleft",
   plugins: [vue()],
   resolve: {
     alias: {
