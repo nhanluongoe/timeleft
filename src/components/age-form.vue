a7824424a65f73eafb1b0202d31368dbbcda790b
nhanluongoe <nhanlt18@gmail.com>
2024-04-28 12:59:32 +0700

    feat: automatically focus to inputs

--- src/components/age-form.vue
+++ src/components/age-form.vue
@@ -10,9 +10,15 @@ const step = ref<Step>("age");
 const age = ref<number>();
 const expectedAge = ref<number>();
 
-function handleFirstStep(value: number | undefined) {
+const expectedAgeInputRef = ref<HTMLInputElement>();
+
+function handleFirstStep(value: number | undefined, event: Event) {
+    event.preventDefault();
     emit("setAge", value);
     step.value = "expected-age";
+    setTimeout(() => {
+        expectedAgeInputRef.value?.focus();
+    }, 100)
 }
 
 function complete(value: number | undefined) {
@@ -43,28 +49,26 @@ function goBack() {
         <button class="link-btn absolute top-2 left-0" :class=" { invisible: step === 'age' } " @click=" goBack ">←
             Back</button>
         <Transition name="slide-fade">
-
-            <div v-if="step === 'age'" class="flex flex-col gap-5 justify-center items-center">
-                <div>
+            <form v-if="step === 'age'" class="flex flex-col gap-5 justify-center items-center">
+                <div class="flex flex-col justify-center items-center">
                     <label for="age" class="input-label">What is your age?</label>
-                    <input id="age" type="number" v-model=" age " class="input" min="0" max="150"></input>
+                    <input id="age" type="number" v-model=" age " class="input" min="0" max="150" autofocus></input>
                 </div>
-                <button @click="handleFirstStep(age)" class="primary-btn" :class=" { 'disabled-btn': !age } "
+                <button @click="handleFirstStep(age, $event)" class="primary-btn" :class=" { 'disabled-btn': !age } "
                     :disabled=" !age ">Next</button>
-            </div>
+            </form>
         </Transition>
 
         <Transition name="slide-fade">
-
-            <div v-if="step === 'expected-age'" class="flex flex-col gap-5 justify-center items-center">
-                <div>
+            <form v-if="step === 'expected-age'" class="flex flex-col gap-5 justify-center items-center">
+                <div class="flex flex-col justify-center items-center">
                     <label for="expected-age" class="input-label">How old do you expect to live?</label>
-                    <input id="expected-age" type="number" v-model=" expectedAge " class="input" min="0"
-                        max="150"></input>
+                    <input id="expected-age" type="number" v-model=" expectedAge " class="input" min="0" max="150"
+                        autofocus ref="expectedAgeInputRef"></input>
                 </div>
                 <button @click="complete(expectedAge)" class="primary-btn" :class=" { 'disabled-btn': !expectedAge } "
                     :disabled=" !expectedAge ">Let's see!</button>
-            </div>
+            </form>
         </Transition>
     </div>
 </template>
